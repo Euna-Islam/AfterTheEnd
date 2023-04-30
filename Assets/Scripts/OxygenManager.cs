ee7be34a92e5f61bb2655f169741abd6a9ad17a7
Euna Islam <euna.islam@ut.ee>
2023-04-30 11:58:12 +0300

    Fixed time and oxygen manager

--- Assets/Scripts/OxygenManager.cs
+++ Assets/Scripts/OxygenManager.cs
@@ -5,6 +5,9 @@ public class OxygenManager : MonoBehaviour
 {
     private static OxygenManager instance;
     public static OxygenManager Instance { get { return instance; } }
+
+    public float OxygenLossRate = 0.0005f;
+    public float OxygenGainRate = 0.0005f;
     void Awake()
     {
         if (instance == null)
@@ -30,11 +33,10 @@ public class OxygenManager : MonoBehaviour
 
     void UpdateOxygenIndicator()
     {
-        float oxygenLossRate = 0.0005f;
-        float oxygenGainRate = 0.0005f;
+        
         if (PlayerMovement.Instance.IsInPollutedArea)
-            OxygenLevel.fillAmount -= oxygenLossRate;
-        else OxygenLevel.fillAmount += oxygenGainRate;
+            OxygenLevel.fillAmount -= OxygenLossRate;
+        else OxygenLevel.fillAmount += OxygenGainRate;
 
         if (GameManager.Instance.IsGamePlaying() && OxygenLevel.fillAmount < .1)
         {
